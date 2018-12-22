57a53ffdd2277afcf3071695cef030b6c6e161ad
Lucas <lucas@MacBook-Air-de-Lucas.local>
2018-12-22 13:08:32 +0100

    loteria de Navidad

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const app = express();
 
 
@@ -14,6 +15,24 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes go here!
+
+app.post('/', (req, res, next) => {
+  axios.get(`http://api.elpais.com/ws/LoteriaNavidadPremiados?n=${req.body.number}`)
+    .then(response => {
+      const json = JSON.parse(response.data.split('=')[1])
+
+      console.log(`NÚMERO: ${json.numero}`)
+      console.log(`PREMIO: ${json.premio}`)
+      
+      res.render('result', {premio: json.premio})
+    })
+})
+
+
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+})
 
 app.listen(PORT, () => {
   console.info(`App listen at ${PORT} port`);
